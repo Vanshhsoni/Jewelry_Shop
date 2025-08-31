2458fb1c0d4bdc8ea4b407e1a119ae93aff1a448
Vanshhsoni <sonivanshmaster@gmail.com>
2025-08-31 16:08:52 +0530

    deleted

--- jewelry_shop/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "core"   # 👈 this prevents NoReverseMatch
-
-urlpatterns = [
-    path("", views.landing_page, name="landing"),
-    path("profile/", views.profile_page, name="profile"),
-]
