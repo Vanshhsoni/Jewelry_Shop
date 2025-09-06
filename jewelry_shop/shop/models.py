569a3f127189b9ab343f3404be3c900ed3c0468c
Vanshhsoni <sonivanshmaster@gmail.com>
2025-09-06 17:22:52 +0530

    Update models.py6

--- jewelry_shop/shop/models.py
+++ jewelry_shop/shop/models.py
@@ -1,8 +1,11 @@
+# From cloudinary.models import CloudinaryField  # This is for the `cloudinary` library
+from cloudinary_storage.models import CloudinaryField  # This is for the `django-cloudinary-storage` library
+
 from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    image = models.ImageField(upload_to="categories/")  # category thumbnail/banner
+    image = CloudinaryField('image')  # Replaced ImageField with CloudinaryField
 
     def __str__(self):
         return self.name
@@ -11,11 +14,11 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
     name = models.CharField(max_length=200)
-    image = models.ImageField(upload_to="products/")  # main product image
+    image = CloudinaryField('image')  # Replaced ImageField with CloudinaryField
     description = models.TextField(blank=True)
     is_available = models.BooleanField(default=True)
     is_featured = models.BooleanField(default=False)
-    price = models.DecimalField(max_digits=10, decimal_places=2)  # ₹9999.99 tak
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
